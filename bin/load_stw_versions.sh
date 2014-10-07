e3a8c52fca0bc339c19dcf1bf02fedd13c02df1c
Joachim Neubert <j.neubert@zbw.eu>
2014-10-07 14:53:26 +0200

    Change URI for dataset versioning ontology

--- bin/load_stw_versions.sh
+++ bin/load_stw_versions.sh
@@ -62,7 +62,7 @@ PREFIXES="
 prefix : <http://raw.github.com/jneubert/skos-history/master/skos-history.ttl/>
 prefix dc: <http://purl.org/dc/elements/1.1/>
 prefix dcterms: <http://purl.org/dc/terms/>
-prefix dsv: <https://raw.github.com/JohanDS/Dataset-versioning--for-KOS-data-sets-/master/DataSetVersioning.owl#>
+prefix dsv: <http://purl.org/iso25964/DataSet/Versioning#>
 prefix owl: <http://www.w3.org/2002/07/owl#>
 prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
